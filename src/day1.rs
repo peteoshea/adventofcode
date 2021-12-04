3252cad1d2d3bbf28cb914f08d4f7c28c783cb2d
Pete O'Shea <mail@peteoshea.co.uk>
2021-12-04 09:06:32 +0000

    Refactor code now it's passing tests

--- src/day1.rs
+++ src/day1.rs
@@ -4,29 +4,25 @@ pub fn run() {
 
 #[allow(dead_code)]
 fn depth_increase(depth1: u32, depth2: u32) -> bool {
-  let mut result = false;
-  if depth2 > depth1 {
-    result = true;
-  }
-  result
+    depth2 > depth1
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_depth_increase_with_same_values_returns_false() {
-    assert!(!depth_increase(200, 200));
-  }
+    #[test]
+    fn test_depth_increase_with_same_values_returns_false() {
+        assert!(!depth_increase(200, 200));
+    }
 
-  #[test]
-  fn test_depth_increase_with_decreasing_depth_returns_false() {
-    assert!(!depth_increase(200, 190));
-  }
+    #[test]
+    fn test_depth_increase_with_decreasing_depth_returns_false() {
+        assert!(!depth_increase(200, 190));
+    }
 
-  #[test]
-  fn test_depth_increase_with_increasing_depth_returns_truee() {
-    assert!(depth_increase(190, 200));
-  }
+    #[test]
+    fn test_depth_increase_with_increasing_depth_returns_true() {
+        assert!(depth_increase(190, 200));
+    }
 }
