3ea173637b3bd0c7ea3814a35a584951bdc79174
Pete O'Shea <mail@peteoshea.co.uk>
2021-12-05 08:31:22 +0000

    Part 2 of day 1

--- src/day1.rs
+++ src/day1.rs
@@ -2,16 +2,18 @@ use super::common;
 
 pub fn run() {
     let lines = common::file_to_vec("./data/day1/input.txt").unwrap();
-    let depths: Vec<u16> = lines.into_iter().map(|line| line.parse().unwrap()).collect();
-    println!("Day1: {}", increase_count(depths));
+    let depths: Vec<u16> = lines
+        .into_iter()
+        .map(|line| line.parse().unwrap())
+        .collect();
+    println!("Day1: part 1 = {}, part 2 = {}", increase_count(&depths), increase_count(&calculate_sliding_windows(&depths)));
 }
 
 fn depth_increase(depth1: u16, depth2: u16) -> bool {
     depth2 > depth1
 }
 
-#[allow(dead_code)]
-fn increase_count(depths: Vec<u16>) -> u16 {
+fn increase_count(depths: &Vec<u16>) -> u16 {
     let mut count = 0;
     // Don't initialise first depth as will be written to on first loop
     let mut depth1: u16;
@@ -25,6 +27,33 @@ fn increase_count(depths: Vec<u16>) -> u16 {
         }
     }
     count
+}
+
+fn calculate_sliding_windows(depths: &Vec<u16>) -> Vec<u16> {
+    const MEASUREMENT_WINDOW: usize = 3;
+    let mut result: Vec<u16> = Vec::new();
+    let max_window_index = depths.len() - MEASUREMENT_WINDOW;
+    for (current_index, depth) in depths.iter().enumerate() {
+        if current_index <= max_window_index {
+            // println!("Push element at index {}", current_index);
+            result.push(*depth);
+        }
+        for index_offset in 1..MEASUREMENT_WINDOW {
+            // println!(
+            //     "Current index {}, index offset {}",
+            //     current_index, index_offset
+            // );
+            if index_offset <= current_index {
+                let window_index = current_index - index_offset;
+                if window_index <= max_window_index {
+                    // println!("Add {} to index {}", depth, window_index);
+                    result[window_index] += depth;
+                }
+            }
+        }
+    }
+    // println!("{:?}", result);
+    result
 }
 
 #[cfg(test)]
@@ -49,12 +78,25 @@ mod tests {
     #[test]
     fn test_increase_count_with_no_increases_returns_zero() {
         let depths: Vec<u16> = vec![199, 198, 197];
-        assert_eq!(increase_count(depths), 0);
+        assert_eq!(increase_count(&depths), 0);
     }
 
     #[test]
     fn test_increase_count_with_example_returns_seven() {
         let depths: Vec<u16> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        assert_eq!(increase_count(depths), 7);
+        assert_eq!(increase_count(&depths), 7);
+    }
+
+    #[test]
+    fn test_sliding_window_returns_vector() {
+        let depths: Vec<u16> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let expected: Vec<u16> = vec![607, 618, 618, 617, 647, 716, 769, 792];
+        assert_eq!(calculate_sliding_windows(&depths), expected);
+    }
+
+    #[test]
+    fn test_increase_count_with_sliding_window_example_returns_five() {
+        let depths: Vec<u16> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(increase_count(&calculate_sliding_windows(&depths)), 5);
     }
 }
