e5c9f8bc5b4af02a22557eb9008d7ae6f8e5b88c
Pete O'Shea <mail@peteoshea.co.uk>
2021-12-07 18:38:16 +0000

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -3,21 +3,36 @@ mod calc;
 
 pub fn run() {
     let lines = common::file_to_vec("./data/day3/input.txt").unwrap();
-    let (gamma, epsilon) = calculate(&lines);
+    let (gamma, epsilon) = calculate_power_consumption(&lines);
     println!(
         "Day3: part 1 = gamma rate: {}, epsilon rate: {} => {}",
         gamma,
         epsilon,
         gamma * epsilon
     );
+    let (oxygen_generator, co2_scrubber) = calculate_life_support_rating(&lines);
+    println!(
+        "Day3: part 2 = oxygen generator rating: {}, CO2 scrubber rating: {} => {}",
+        oxygen_generator,
+        co2_scrubber,
+        oxygen_generator * co2_scrubber
+    );
 }
 
-fn calculate(diagnostics: &Vec<String>) -> (u32, u32) {
-    let (gamma_rate, epsilon_rate) = calc::calculate_rates(diagnostics);
-    (
-      calc::convert_bits_to_decimal(gamma_rate),
-      calc::convert_bits_to_decimal(epsilon_rate),
-    )
+fn calculate_power_consumption(diagnostics: &Vec<String>) -> (u32, u32) {
+  let (gamma_rate, epsilon_rate) = calc::calculate_power_rates(diagnostics);
+  (
+    calc::convert_bits_to_decimal(gamma_rate),
+    calc::convert_bits_to_decimal(epsilon_rate),
+  )
+}
+
+fn calculate_life_support_rating(diagnostics: &Vec<String>) -> (u32, u32) {
+  let (oxygen_generator, co2_scrubber) = calc::calculate_life_support_values(diagnostics);
+  (
+    calc::convert_bits_to_decimal(oxygen_generator),
+    calc::convert_bits_to_decimal(co2_scrubber),
+  )
 }
 
 #[cfg(test)]
@@ -25,7 +40,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_calculate_with_example_returns_expected_values() {
+    fn test_calculate_life_support_rating_with_example_returns_expected_values() {
         let diagnostics: Vec<String> = vec![
             "00100".to_string(),
             "11110".to_string(),
@@ -40,6 +55,25 @@ mod tests {
             "00010".to_string(),
             "01010".to_string(),
         ];
-        assert_eq!(calculate(&diagnostics), (22, 9));
+        assert_eq!(calculate_life_support_rating(&diagnostics), (23, 10));
+    }
+
+    #[test]
+    fn test_calculate_power_consumption_with_example_returns_expected_values() {
+        let diagnostics: Vec<String> = vec![
+            "00100".to_string(),
+            "11110".to_string(),
+            "10110".to_string(),
+            "10111".to_string(),
+            "10101".to_string(),
+            "01111".to_string(),
+            "00111".to_string(),
+            "11100".to_string(),
+            "10000".to_string(),
+            "11001".to_string(),
+            "00010".to_string(),
+            "01010".to_string(),
+        ];
+        assert_eq!(calculate_power_consumption(&diagnostics), (22, 9));
     }
 }
--- src/day3/calc.rs
+++ src/day3/calc.rs
@@ -6,7 +6,7 @@ fn split_diagnotic_into_bits(diagnostic: String) -> Vec<u32> {
     result
 }
 
-pub fn calculate_rates(diagnostics: &Vec<String>) -> (Vec<u32>, Vec<u32>) {
+pub fn calculate_power_rates(diagnostics: &Vec<String>) -> (Vec<u32>, Vec<u32>) {
     let mut gamma_rate = Vec::new();
     let mut epsilon_rate = Vec::new();
     let mut bit_counts: Vec<u32> = Vec::new();
@@ -44,26 +44,143 @@ pub fn calculate_rates(diagnostics: &Vec<String>) -> (Vec<u32>, Vec<u32>) {
 }
 
 fn invert_bit(bit: u32) -> u32 {
-  let mut result = 0;
-  if bit == 0 {
-      result = 1;
-  }
-  result
+    let mut result = 0;
+    if bit == 0 {
+        result = 1;
+    }
+    result
 }
 
 pub fn convert_bits_to_decimal(bits: Vec<u32>) -> u32 {
-  let mut bit_multiplier: u32 = 1;
-  let mut result = 0;
-  for bit in bits.iter().rev() {
-      result += bit * bit_multiplier;
-      bit_multiplier *= 2;
-  }
-  result
+    let mut bit_multiplier: u32 = 1;
+    let mut result = 0;
+    for bit in bits.iter().rev() {
+        result += bit * bit_multiplier;
+        bit_multiplier *= 2;
+    }
+    result
+}
+
+pub fn calculate_life_support_values(diagnostics: &Vec<String>) -> (Vec<u32>, Vec<u32>) {
+    let mut stripped_list = diagnostics.to_vec();
+    let mut bit_index: usize = 0;
+    // println!("Oxygen calculation:");
+    while stripped_list.len() > 1 {
+        let (gamma, _epsilon) = calculate_power_rates(&stripped_list);
+        let bit_value = gamma[bit_index];
+        // println!("Bit_index: {} value: {}", bit_index, bit_value);
+        stripped_list = strip_items(&stripped_list, bit_index, bit_value);
+        // println!("Updated list: {:?}", stripped_list);
+        bit_index += 1;
+    }
+    let oxygen_generator = split_diagnotic_into_bits(stripped_list[0].to_string());
+    stripped_list = diagnostics.to_vec();
+    bit_index = 0;
+    // println!("CO2 calculation:");
+    while stripped_list.len() > 1 {
+        let (_gamma, epsilon) = calculate_power_rates(&stripped_list);
+        let bit_value = epsilon[bit_index];
+        // println!("Bit_index: {} value: {}", bit_index, bit_value);
+        stripped_list = strip_items(&stripped_list, bit_index, bit_value);
+        // println!("Updated list: {:?}", stripped_list);
+        bit_index += 1;
+    }
+    let co2_scrubber = split_diagnotic_into_bits(stripped_list[0].to_string());
+    (oxygen_generator, co2_scrubber)
+}
+
+fn strip_items(diagnostics: &Vec<String>, bit_index: usize, value: u32) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for diagnostic in diagnostics.iter() {
+        let bits = split_diagnotic_into_bits(diagnostic.to_string());
+        if bits[bit_index] == value {
+            result.push(diagnostic.to_string());
+        }
+    }
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_strip_items_for_first_bit_with_example_data_returns_expected_vector() {
+        let diagnostics: Vec<String> = vec![
+            "00100".to_string(),
+            "11110".to_string(),
+            "10110".to_string(),
+            "10111".to_string(),
+            "10101".to_string(),
+            "01111".to_string(),
+            "00111".to_string(),
+            "11100".to_string(),
+            "10000".to_string(),
+            "11001".to_string(),
+            "00010".to_string(),
+            "01010".to_string(),
+        ];
+        let bit_index: usize = 0;
+        let bit_value: u32 = 1;
+        assert_eq!(
+            strip_items(&diagnostics, bit_index, bit_value),
+            vec![
+                "11110".to_string(),
+                "10110".to_string(),
+                "10111".to_string(),
+                "10101".to_string(),
+                "11100".to_string(),
+                "10000".to_string(),
+                "11001".to_string(),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_strip_items_for_second_bit_with_example_data_returns_expected_vector() {
+        let diagnostics: Vec<String> = vec![
+            "11110".to_string(),
+            "10110".to_string(),
+            "10111".to_string(),
+            "10101".to_string(),
+            "11100".to_string(),
+            "10000".to_string(),
+            "11001".to_string(),
+        ];
+        let bit_index: usize = 1;
+        let bit_value: u32 = 0;
+        assert_eq!(
+            strip_items(&diagnostics, bit_index, bit_value),
+            vec![
+                "10110".to_string(),
+                "10111".to_string(),
+                "10101".to_string(),
+                "10000".to_string(),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_calculate_life_support_values_with_example_data_returns_expected_bits() {
+        let diagnostics: Vec<String> = vec![
+            "00100".to_string(),
+            "11110".to_string(),
+            "10110".to_string(),
+            "10111".to_string(),
+            "10101".to_string(),
+            "01111".to_string(),
+            "00111".to_string(),
+            "11100".to_string(),
+            "10000".to_string(),
+            "11001".to_string(),
+            "00010".to_string(),
+            "01010".to_string(),
+        ];
+        assert_eq!(
+            calculate_life_support_values(&diagnostics),
+            (vec![1, 0, 1, 1, 1], vec![0, 1, 0, 1, 0])
+        );
+    }
 
     #[test]
     fn test_convert_bits_to_decimal_with_example_gamma_returns_22() {
@@ -81,7 +198,7 @@ mod tests {
     }
 
     #[test]
-    fn test_calculate_rates_with_example_returns_expected_bits() {
+    fn test_calculate_power_rates_with_example_returns_expected_bits() {
         let diagnostics: Vec<String> = vec![
             "00100".to_string(),
             "11110".to_string(),
@@ -96,7 +213,10 @@ mod tests {
             "00010".to_string(),
             "01010".to_string(),
         ];
-        assert_eq!(calculate_rates(&diagnostics), (vec![1, 0, 1, 1, 0], vec![0, 1, 0, 0, 1]));
+        assert_eq!(
+            calculate_power_rates(&diagnostics),
+            (vec![1, 0, 1, 1, 0], vec![0, 1, 0, 0, 1])
+        );
     }
 
     #[test]
